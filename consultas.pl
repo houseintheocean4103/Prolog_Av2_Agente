6540ce79c2056b51c81fe0b798b76be2530e96ad
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2025-09-07 19:50:29 -0300

    Create consultas.pl

---
+++ consultas.pl
@@ -0,0 +1,18 @@
+? - inicio(estacao), 
+    vende(estacao),
+    comprar(sorvete), 
+    esta_conectado(estacao), 
+    mover_para(republica),
+    vende(republica), 
+    comprar(livro), comprar(fruta), 
+    ver_compras, 
+    mover_para(patiobelem), 
+    ver_visitados, comprar(roupa), 
+    devolver(livro), 
+    mover_para(republica), 
+    estou, devolver(livro), 
+    final, 
+    !. 
+
+
+? - tutorial.
